f17a0461ee3e8f88255d60b75607a4b1c70273a5
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-14 16:59:37 +0300

    ..

--- main.py
+++ main.py
@@ -22,24 +22,6 @@ def start_message(message):
     response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
     bot.send_message(message.chat.id, response.text)
 
-@bot.message_handler(commands=['quote'])
-def quote_command(message):
-    response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
-    bot.send_message(message.chat.id, response.text)
-    makeup_inline = types.InlineKeyboardMarkup()
-    item_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
-    item_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
-
-    makeup_inline.add(item_yes,item_no)
-    bot.send_message(message.chat.id, 'Вам нравится эта цитата?',
-                     reply_markup= makeup_inline
-                     )
-
-def answer(call):
-    if call.data == 'yes':
-        pass
-    else:
-        pass
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
