1d5b002b481caecdd7da908f330af8c4ce72caf6
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-14 18:45:41 +0300

    ..

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ bot = telebot.TeleBot(BOT_TOKEN)
 response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
 print(response.text)
 
-@bot.message_handler(commands=['quote'])
-def start_message(message):
-    response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
-    bot.send_message(message.chat.id, response.text)
-
 
 @bot.message_handler(commands=['start'])
 def quote_message(message):
@@ -36,11 +31,18 @@ def quote_message(message):
 @bot.callback_query_handler(func= lambda call: True)
 def answer(call):
     if call.data == 'yes':
-        bot.reply_to(call, call.data)
+        answer = 'yes'
     else:
-        bot.reply_to(call, call.data)
+        answer = 'no'
+    bot.send_message(call.message.chat.id, answer)
 
-
+@bot.message_handler(commands=['test'])
+def start_message(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Три', callback_data=3))
+    markup.add(telebot.types.InlineKeyboardButton(text='Четыре', callback_data=4))
+    markup.add(telebot.types.InlineKeyboardButton(text='Пять', callback_data=5))
+    bot.send_message(message.chat.id, text="Какая средняя оценка была у Вас в школе?", reply_markup=markup)
 
 
 bot.polling(none_stop=True)
