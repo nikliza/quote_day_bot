987b39a55c64a7702faeb2aa5006cc89d2707f9b
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-14 16:55:06 +0300

    ..

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ from telebot import types
 
 bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
-logger = telebot.logger
-logger.setLevel(logging.DEBUG)
+
 
 
 response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
@@ -36,23 +35,8 @@ def quote_command(message):
                      reply_markup= makeup_inline
                      )
 
-@bot.callback_query_handlers(func = lambda call: True)
-
 def answer(call):
     if call.data == 'yes':
         pass
     else:
         pass
-
-
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
