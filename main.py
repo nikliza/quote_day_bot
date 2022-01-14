d8e9eb757cae8c07346ac70d139f4de0adafd4ac
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-14 13:16:00 +0300

    ..

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import requests
+import os
 import telebot
-bot = telebot.TeleBot("5025840937:AAETRY14UP2PPgmf3v_MUW8KxSup2An_CUA")
+import logging
+import psycopg2
+import requests
+from config import *
+from flask import Flask, request
+
+
+bot = telebot.TeleBot(BOT_TOKEN)
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
 
 response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
 print(response.text)
@@ -9,3 +20,8 @@ print(response.text)
 def start_message(message):
     response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
     bot.send_message(message.chat.id, response.text)
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
