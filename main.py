31e53735bb16fd648dad5b57cffec16fcd2eee47
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-14 17:32:12 +0300

    ..

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ from config import *
 from flask import Flask, request
 from telebot import types
 
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
 
 response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
 print(response.text)
@@ -23,5 +22,16 @@ def start_message(message):
     response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
     bot.send_message(message.chat.id, response.text)
 
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port = int(os.environ.get("PORT", 5000)))
 
