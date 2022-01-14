ad1e09583fe999a507f93669e9a8bf0662592914
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-14 13:18:28 +0300

    ..

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ def start_message(message):
     response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
     bot.send_message(message.chat.id, response.text)
 
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
 if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
