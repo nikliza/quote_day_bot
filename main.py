5a151afe055a49ad3f4546f5fe3badb7c9272bf4
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-15 11:54:54 +0300

    ..

--- main.py
+++ main.py
@@ -6,13 +6,27 @@ import telebot
 
 
 bot = telebot.TeleBot(TOKEN)
-
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello')
 
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
-bot.polling()
 
+@server.route(f"/{TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port = int(os.environ.get("PORT", 5000)))
