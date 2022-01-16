dcc7ebd0eecece099f85984c9bd6c410be9af157
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-16 16:57:42 +0300

    ..

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-TOKEN = "5025840937:AAETRY14UP2PPgmf3v_MUW8KxSup2An_CUA"
+TOKEN = "5065035672:AAE6XZcRPVSMOWpSu785c84yBbfnqRzfGWI"
 APP_URL = "https://mostsimplebot.herokuapp.com/" + TOKEN
 DB_URI = "postgres://bmasxluidbjroh:e6d1988e8d80528d111c2879613e98086af9a33bfb3f7ac4f110060277ce95ef@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/d8frdp6c8pmb6e"
+HOST = "ec2-52-208-221-89.eu-west-1.compute.amazonaws.com"
+User = "bmasxluidbjroh"
+Database = "d8frdp6c8pmb6e"
+Password = "e6d1988e8d80528d111c2879613e98086af9a33bfb3f7ac4f110060277ce95ef"
--- main.py
+++ main.py
@@ -1,63 +1,48 @@
 import os
-from telebot import types
-import requests
-from flask import Flask, request
-from config import *
 import telebot
+import logging
+import psycopg2
+import requests
+from config import *
+from flask import Flask, request
+from telebot import types
 
+db_connection = psycopg2.connect(
+    host =HOST,
+    user = User,
+    password = Password,
+    database = Database
+)
+db_object = db_connection.cursor()
 
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+
+def update_messages_count(user_id):
+    db_connection = psycopg2.connect(DB_URI, sslmode="require")
+    db_object = db_connection.cursor()
+    db_object.execute(f"UPDATE users SET messages = messages + 1 WHERE id = {user_id}")
+    db_connection.commit()
 
 
-@bot.message_handler(commands=['start'])
-def quote_message(message):
-    response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
-    bot.send_message(message.chat.id, response.text)
-    makup_inline = types.InlineKeyboardMarkup()
-    item_yes = types.InlineKeyboardButton(text = 'ДА', callback_data='yes')
-    item_no = types.InlineKeyboardButton(text='НЕТ', callback_data='no')
+@bot.message_handler(commands=["start"])
+def start(message):
+    def quote_message(message):
+        response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text')
+        bot.send_message(message.chat.id, response.text)
 
-    makup_inline.add(item_yes, item_no)
-    bot.send_message(message.chat.id, 'Вам понравилась цитата?',
-                     reply_markup=makup_inline
-                     )
+    user_id = message.from_user.id
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello, {username}!")
+    db_connection = psycopg2.connect(DB_URI, sslmode="require")
+    db_object = db_connection.cursor()
+    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    result = db_object.fetchone()
 
-@bot.callback_query_handler(func= lambda call: True)
-def answer(call):
-    if call.data == 'yes':
-        makoup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item_live = types.KeyboardButton('Жизнь')
-        item_l0ve = types.KeyboardButton('Любовь')
+    if not result:
+        db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
+        db_connection.commit()
 
-        makoup_reply.add(item_live, item_l0ve)
-        bot.send_message(call.message.chat.id, 'Какая у цитаты тема?',
-                         reply_markup=makoup_reply)
-    else:
-        answer = 'no'
-    bot.send_message(call.message.chat.id, answer)
-
-@bot.message_handler(content_types=['text'])
-def get_text(message):
-    if message.text == 'Жизнь':
-        bot.send_message(message.chat.id, 'жизнь')
-    else:
-        bot.send_message(message.chat.id, 'любовь')
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+    update_messages_count(user_id)
 
 
-@server.route(f"/{TOKEN}", methods=["POST"])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port = int(os.environ.get("PORT", 5000)))
+bot.polling()
