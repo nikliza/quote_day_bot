25bad78a1154a3eb45c567d5cfc11cfb3df5ff1c
Elizaveta <lizza.nikkitina@gmail.com>
2022-01-17 13:29:25 +0300

    ..

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ db_connection = psycopg2.connect(
 )
 
 bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
 def quota_text():
@@ -244,4 +245,19 @@ def select(message):
     bot.send_message(message.chat.id, 'Выберите настроение?',
                      reply_markup=makup_inline4
                      )
-bot.polling(none_stop=True)
+
+
+
+
+@server.route(f"/{TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
